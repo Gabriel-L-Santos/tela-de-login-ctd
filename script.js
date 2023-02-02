50d68b695657cd1822a778858d93574a488c663b
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-02-01 23:06:43 -0300

    30º dia de CTD - edit no js

--- script.js
+++ script.js
@@ -1,26 +1,92 @@
-function signup() {
-  var username = document.getElementById("signup-username").value;
-  var password = document.getElementById("signup-password").value;
-  var passwordConfirm = document.getElementById("signup-password-confirm").value;
-  if (password === passwordConfirm) {
+function cadastrar() {
+  let usuario = this.lerDadosCadastro();
+  
+  if (validarCamposCadastro(usuario) == true) {    
+    window.location.href = "index.html";
+    sessionStorage.setItem("nomeCompleto", usuario.nomeCompleto);
+    sessionStorage.setItem("login", usuario.email);
+    sessionStorage.setItem("senha", usuario.senha);
+    sessionStorage.setItem("confirmarSenha", usuario.confirmarSenha);
+  }else {
+
+  }
+
+}
+function logar() {
+  let dadosLogin = this.lerDadosLogin();
+  //Pegando os dados de login que o usuário cadastrou pelo SessionStorage e guardando nas variáveis email e senha.
+  login.loginBD = sessionStorage.getItem("login")
+  login.senhaBD = sessionStorage.getItem("senha")
+
+  if (validarCamposLogin(dadosLogin) == true) {
     
-    username != password;
-    alert("Conta criada com sucesso!");
-  } else {
-    alert("Senhas não correspondem!");
+      if (dadosLogin.email === login.loginBD && dadosLogin.senha === login.senhaBD) {
+        window.location.href = "assets/pages/home.html";
+      }else
+        alert('Login incorreto!')
+  }else {
+
   }
+
 }
 
-function login() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-
-  if (username != password) {
-    alert("Login feito com sucesso!");
-  } else {
-    alert("Senha ou nome de usuário incorreta");
+function validarCamposLogin(login) {
+  let msg = ''
+  
+  if (!login.email) {
+    msg += 'Informe o seu usuário de Login\n'
   }
+  if (!login.senha) {
+    msg += 'Informe a sua senha \n'  
+  if (msg != '') {
+    alert(msg)
+    return false
+  }
+    
+  }
+  return true
 }
+
+function validarCamposCadastro(usuario) {
+  let msg = ''
+
+  if (!usuario.nomeCompleto) {
+    msg += 'Informe o seu nome completo \n'
+  }
+  if (!usuario.email) {
+    msg += 'Informe o seu Login de usuário \n'
+  }
+  if (!usuario.senha) {
+    msg += 'Informe a sua senha \n'
+  }
+    if (!usuario.confirmarSenha) {
+  msg += 'Confirme a sua senha \n'
+  }
+  if (usuario.senha != usuario.confirmarSenha) {
+    msg += 'As senhas não conferem'
+  }
+  if (msg != '') {
+    alert(msg)
+    return false
+  }
+    return true
+  }
+
+  function lerDadosLogin() {
+    let login = {}
+      login.email = document.getElementById('login').value
+      login.senha = document.getElementById('senha').value
+    return login;
+  }
+
+  function lerDadosCadastro() {
+    let usuario = {}
+      usuario.nomeCompleto = document.getElementById('nomeCompleto').value
+      usuario.email = document.getElementById('email').value
+      usuario.senha = document.getElementById('senha').value
+      usuario.confirmarSenha = document.getElementById('confirmarSenha').value
+    return usuario;    
+  }
 
 function switchToSignUp() {
   // esconder a tela de login
